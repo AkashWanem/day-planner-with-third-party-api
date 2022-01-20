e9969e79b55171b905be6eda0c1a99429db9b541
Akash Limbu <akashwanem@hotmail.com>
2022-01-20 16:38:08 +0000

    trying to write change color function

--- Develop/Javascipt/script.js
+++ Develop/Javascipt/script.js
@@ -1,3 +1,27 @@
+$(document).ready(function() {
+    //console.log("hello");
+    
+    //Display current date and time
+    let date = moment().format('dddd MMMM Do YYYY, h:mm');
+    $('#currentDay').text(date);
 
-let date = moment().format('dddd MMMM Do');
-$('#currentDay').text(date);
+    // color coding each time block
+    function changeColor() {
+        const d = new Date();
+        let hour = d.getHours();
+        for (let i = 0; i <= 9; i++) {
+            console.log(hour, i)
+            if (hour === i) {
+                $('.description').css("background", "red")
+            } else if (hour < i) {
+                $('.description').css("background", "lightblue")
+            } else {
+                $('.description').css("background", "#eee")
+            }
+        }
+    }
+
+    setInterval(function(){
+        changeColor();
+    },1000)
+});
